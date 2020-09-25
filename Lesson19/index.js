e61cffa082e83c9bdd9bfe19a2b04ff643362fb1
sayani-github12 <sayani.chowdhury1@gmail.com>
2020-09-25 15:46:25 -0400

    https://github.com/sayani-github12/Comit_ReactJSCourse.git

---
+++ Lesson19/index.js
@@ -0,0 +1,16 @@
+const fName="John";
+const lName="Paul";
+const dateOfBirth="August";
+
+let city;
+let age;
+let next_city;
+
+city="Kitchener";
+age=29;
+
+next_city=city;
+
+console.log("Hi my name is:",fName,lName ,".I am",age ,"old","I am staying  in",city);
+
+console.log('First Name ' + fName + '\nLast Name ' + lName+'.');
