24b47555ff17f615bf9ea06a6022bdba73818137
sayani-github12 <sayani.chowdhury1@gmail.com>
2020-09-25 16:10:09 -0400

    https://github.com/sayani-github12/Comit_ReactJSCourse.git

---
+++ lesson_20/index.js
@@ -0,0 +1,12 @@
+let age = "29";
+console.log(typeof age);
+console.log("Age is: "+ Number(age)); // boolean
+let isCompleted= "false";
+console.log(typeof isCompleted);
+console.log(typeof Boolean(isCompleted))
+
+age=123;
+
+console.log(typeof age)
+console.log(typeof String(age))
+
