86dcbcb22b571a74d209b5a7798f7556caaaca5c
sayani-github12 <sayani.chowdhury1@gmail.com>
2020-09-28 20:27:05 -0400

    https://github.com/sayani-github12/Comit_ReactJSCourse.git

--- Lesson 23/index.js
+++ Lesson 23/index.js
@@ -1,15 +1,22 @@
 let curruntHr= Number(prompt("Enter Current hour...."))
 
+if(curruntHr!=0){
+    if (curruntHr<0 || curruntHr>23){
 
- if (curruntHr<0 || curruntHr>23){
+        alert("Input is not correct")
+    
+    }  else if(curruntHr<8 || curruntHr>=18){
+    
+        alert("hour is outside of office hours")
+    }
+  
+    else{
+        alert("we are open")
+    }
 
-    alert("Input is not correct")
 
-}
-else if(curruntHr<8 || curruntHr>=18){
+}else{alert("Put some digit")}
+    
 
-    alert("hour is outside of office hours")
-}
-else{
-    alert("we are open")
-}
+
+   
---
+++ Lesson 25/index.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+let weekdayNumber=prompt("Enter Days.....");
+if(weekdayNumber=>1& weekdayNumber<=7){
+    switch(weekdayNumber){
+        case '1':
+
+            alert("Today is Monday");
+        
+            break;
+        
+            case '2':
+            alert("Today is Monday");
+        
+            break;
+        
+        
+            case '3':
+            alert("Today is Wednesday");
+        
+            break;
+        
+            case '4':
+            alert("Today is Thrusday");
+        
+            break;
+        
+        
+            case '5':
+            alert("Today is Friday");
+        
+            break;
+        
+        
+        case '6':
+            alert("Today is Saturday");
+        
+            break;
+        
+        
+        case '7':
+            alert("Today is Sunday");
+        
+            break;
+          
+        }
+        
+     console.log("Number is not bwtween 1 to 7")   
+
+}
